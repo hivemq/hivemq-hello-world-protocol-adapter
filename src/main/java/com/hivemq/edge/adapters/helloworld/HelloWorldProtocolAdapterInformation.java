5243276220282f4141c280b7cd19452c521b35c0
Daniel Krueger <daniel.krueger@hivemq.com>
2024-06-03 09:11:15 +0200

    expand the explanation of the HelloWorldProtocolAdapterInformation

--- src/main/java/com/hivemq/edge/adapters/helloworld/HelloWorldProtocolAdapterInformation.java
+++ src/main/java/com/hivemq/edge/adapters/helloworld/HelloWorldProtocolAdapterInformation.java
@@ -34,38 +34,44 @@ public class HelloWorldProtocolAdapterInformation implements ProtocolAdapterInfo
 
     public static final @NotNull ProtocolAdapterInformation INSTANCE = new HelloWorldProtocolAdapterInformation();
 
-    // TODO improve Java Doc
-
     protected HelloWorldProtocolAdapterInformation() {
     }
 
     @Override
     public @NotNull String getProtocolName() {
+        // the returned string will be used for logging information on the protocol adapter
         return "Hello World Protocol";
     }
 
     @Override
     public @NotNull String getProtocolId() {
+        // this id is very important as this is how the adapters configurations in the config.xml are linked to the adapter implementations.
+        // any change here means you will need to edit the config.xml
         return "Hello_World_Protocol";
     }
 
     @Override
     public @NotNull String getDisplayName() {
+        // the name for this protocol adapter type that will be displayed within edge's ui
         return "Hello World Protocol Adapter";
     }
 
     @Override
     public @NotNull String getDescription() {
+        // the description that will be shown for this protocol adapter within edge's ui
         return "CHANGE ME: Here the description of your protocol adapter needs to go.";
     }
 
     @Override
     public @NotNull String getUrl() {
+        // this url will be displayed in the ui as a link to further documentation on this protocol adapter.
+        // e.g. this could be a link to the source code and a readme
         return "CHANGE_ME";
     }
 
     @Override
     public @NotNull String getVersion() {
+        // the version of this protocol adapter, the usage of semantic versioning is advised.
         return "0.0.1";
     }
 
@@ -83,6 +89,7 @@ public class HelloWorldProtocolAdapterInformation implements ProtocolAdapterInfo
 
     @Override
     public @NotNull String getAuthor() {
+        // your name/nick
         return "CHANGE_ME";
     }
 
